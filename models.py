313f08897a4f3e977b2f544c306d63d99c2bdde3
rohithsowmithra <rohithsowmithra@gmail.com>
2020-09-10 12:18:31 +0530

    updated Artist model to include website and added a migration for this change

--- models.py
+++ models.py
@@ -31,6 +31,7 @@ class Artist(db.Model):
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
